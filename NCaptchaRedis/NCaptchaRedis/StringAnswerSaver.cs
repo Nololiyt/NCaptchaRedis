361070cfc77cbae9720ea40779a66b7b6bb81c56
yueyinqiu <lujiaqi2003@126.com>
2021-01-22 22:25:30 +0800

    look at me!

---
+++ NCaptchaRedis/NCaptchaRedis/StringAnswerSaver.cs
@@ -0,0 +1,49 @@
+﻿using Nololiyt.Captcha.Interfaces;
+using StackExchange.Redis;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nololiyt.NCaptchaExtensions.Redis
+{
+    public sealed class StringAnswerSaver : ICaptchaAnswerSaver<string>
+    {
+        private readonly ConnectionMultiplexer connectionMultiplexer;
+        private readonly IDatabase database;
+        private readonly string prefix;
+        public StringAnswerSaver(string prefix, TimeSpan? answersLifeTime, string redisConnectionString, int db = -1)
+        {
+            this.AnswersLifeTime = answersLifeTime;
+            this.connectionMultiplexer = ConnectionMultiplexer.Connect(redisConnectionString);
+            this.database = this.connectionMultiplexer.GetDatabase(db);
+            this.prefix = prefix;
+        }
+        public TimeSpan? AnswersLifeTime { get; }
+
+        public void Dispose()
+        {
+            this.connectionMultiplexer.Dispose();
+            GC.SuppressFinalize(this);
+        }
+        public async ValueTask<string> SaveAsync(string answer,
+            CancellationToken cancellationToken = default)
+        {
+            string id;
+            do
+            {
+                id = $"{this.prefix}{Guid.NewGuid():N}";
+            }
+            while (!await this.database.StringSetAsync(
+                id, answer, this.AnswersLifeTime, When.NotExists).ConfigureAwait(false));
+            return id;
+        }
+        public async ValueTask<string?> TryGetAndRemoveAsync(string id,
+            CancellationToken cancellationToken = default)
+        {
+            var result = await this.database.StringGetAsync(id).ConfigureAwait(false);
+            if (result.IsNull)
+                return null;
+            return result;
+        }
+    }
+}
---
+++ NCaptchaRedis/NCaptchaRedis/TicketFactory.cs
@@ -0,0 +1,50 @@
+﻿using Nololiyt.Captcha.Interfaces;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nololiyt.NCaptchaExtensions.Redis
+{
+    public sealed class TicketFactory : ITicketFactory
+    {
+        private readonly ConnectionMultiplexer connectionMultiplexer;
+        private readonly IDatabase database;
+        private readonly string prefix;
+        public TicketFactory(string prefix, TimeSpan? ticketsLifeTime, string redisConnectionString, int db = -1)
+        {
+            this.TicketsLifeTime = ticketsLifeTime;
+            this.connectionMultiplexer = ConnectionMultiplexer.Connect(redisConnectionString);
+            this.database = this.connectionMultiplexer.GetDatabase(db);
+            this.prefix = prefix;
+        }
+        public TimeSpan? TicketsLifeTime { get; }
+
+        public void Dispose()
+        {
+            this.connectionMultiplexer.Dispose();
+            GC.SuppressFinalize(this);
+        }
+        public async ValueTask<string> GenerateNewAsync(CancellationToken cancellationToken = default)
+        {
+            string ticket;
+            do
+            {
+                ticket = $"{this.prefix}{Guid.NewGuid():N}";
+            }
+            while (!await this.database.StringSetAsync(
+                ticket, ticket, this.TicketsLifeTime, When.NotExists).ConfigureAwait(false));
+            return ticket;
+        }
+        public async ValueTask<bool> VerifyAsync(string ticket, CancellationToken cancellationToken = default)
+        {
+            var result = await this.database.StringGetAsync(ticket).ConfigureAwait(false);
+            if (result.IsNull)
+                return false;
+            return (string)result == ticket;
+        }
+    }
+}
